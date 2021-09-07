7d2d7d0b088602e6375df92cdde1e5e4e32ce2ed
SOORYA PRAKASH PARTHIBAN <sooryaprak@outlook.com>
2021-09-07 19:07:02 +1200

    Delete 05_Edible Mushroom Classification directory

--- 05_Edible Mushroom Classification/templates/js/app.js
+++
@@ -1,8 +0,0 @@
-<!--JAVASRRIPT-->
-    <script type="text/javascript">
-      var loadFile = function(event) {
-      var image = document.getElementById('output');
-      image.src = URL.createObjectURL(event.target.files[0]);
-    };
-    </script>
- <!--END OF JAVASRRIPT-->
